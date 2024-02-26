b179f59918db64acff177ae986deb0cf79cac80e
Jhonny Knaak de Vargas <jhonny.knaak@gmail.com>
2024-02-25 22:24:35 -0800

    still a testing failing.

--- maximum_score.cpp
+++ maximum_score.cpp
@@ -16,23 +16,21 @@ int maximum_score::calc(int pos_right, int pos_left, vector<int>& nums, int pos_
 
     auto hash = hashFunction(pos_right, pos_left, pos_mul);
 
-    if (map.contains(hash)) {
+    if (map.contains(hash))
         return map[hash];
-    }
 
     auto left = multipliers[pos_mul] * nums[pos_left] + calc(pos_right, pos_left + 1, nums, pos_mul + 1, multipliers, map);
     auto right = multipliers[pos_mul] * nums[pos_right] + calc(pos_right - 1, pos_left, nums, pos_mul + 1, multipliers, map);
 
-    map[hash] = max(left, right);
-    return map[hash];
+    return map[hash] = max(left, right);
 }
 
 std::size_t maximum_score::hashFunction(int pos_right, int pos_left, int pos_mul) {
     std::size_t hash_value = 0;
 
-    hash_value += 3 * std::hash<int>{}(pos_right);
-    hash_value += 5 * std::hash<int>{}(pos_left);
-    hash_value += 7 * std::hash<int>{}(pos_mul);
+    hash_value += 3 * pos_right;
+    hash_value += 5 * pos_left;
+    hash_value += 7 * pos_mul;
 
     return hash_value;
 }

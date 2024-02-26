408ac7549fd4be6c8c93ce14900dfbaffaed5231
Jhonny Knaak de Vargas <jhonny.knaak@gmail.com>
2024-02-25 21:03:38 -0800

    maximal square.

---
+++ maximal_square_tests.cpp
@@ -0,0 +1,18 @@
+//
+// Created by jhonny on 2/25/24.
+//
+
+#include <gtest/gtest.h>
+#include "maximal_square.h"
+
+TEST(maximal_square, TestOne) {
+    auto solution = maximal_square();
+    std::vector<std::vector<char>> matrix = {
+            {'1', '0', '1', '0', '0'},
+            {'1', '0', '1', '1', '1'},
+            {'1', '1', '1', '1', '1'},
+            {'1', '0', '0', '1', '0'}
+    };
+
+    EXPECT_EQ(4, solution.maximalSquare(matrix));
+}

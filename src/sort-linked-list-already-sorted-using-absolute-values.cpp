44f1eb919f7137ed137e0254dd26baa29af5e1f4
Jhonny Knaak de Vargas <jhonny.knaak@gmail.com>
2025-03-30 23:30:51 -0700

    working on it

--- src/sort-linked-list-already-sorted-using-absolute-values.cpp
+++ src/sort-linked-list-already-sorted-using-absolute-values.cpp
@@ -1,6 +1,9 @@
-//
-// Created by jhonny on 3/30/25.
-//
+//https://leetcode.com/problems/sort-linked-list-already-sorted-using-absolute-values/description/?envType=study-plan-v2&envId=tiktok-spring-23-high-frequency
+
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <vector>
 
 struct ListNode {
   int val;
@@ -12,5 +15,63 @@ struct ListNode {
 
 class Solution {
  public:
-  ListNode* sortLinkedList(ListNode* head) { return nullptr; }
+  ListNode* sortLinkedList(ListNode* head) {
+    ListNode* node = head;
+    ListNode* prev = nullptr;
+
+    while (node) {
+      if (prev && prev->val == -1 * node->val) {
+        if (prev->val < node->val) {
+          std::swap(prev->val, node->val);
+        }
+      }
+      if (prev && prev->val > node->val) {
+        prev->next = node->next;
+        node->next = head;
+        head = node;
+        node = prev->next;
+      } else {
+        prev = node;
+        node = node->next;
+      }
+    }
+    return head;
+  }
+
+  ListNode* sortLinkedList_jho(ListNode* head) {
+    std::vector<ListNode*> nodes;
+    auto ptr = head;
+
+    while (ptr != nullptr) {
+      nodes.push_back(ptr);
+      ptr = ptr->next;
+    }
+
+    std::sort(nodes.begin(), nodes.end(), [](ListNode* a, ListNode* b) { return a->val < b->val; });
+
+    for (int i = 0; i < nodes.size(); i++) {
+      if (i + 1 < nodes.size()) {
+        nodes[i]->next = nodes[i + 1];
+      }
+    }
+
+    nodes[nodes.size() - 1]->next = nullptr;
+
+    return nodes[0];
+  }
 };
+
+void free_list_node(ListNode* treeNode) {
+  if (treeNode->next != nullptr) {
+    free_list_node(treeNode->next);
+  }
+  delete treeNode;
+}
+
+TEST(sort_linked_list_already_sorted_using_absolute_values, test_one) {
+  ListNode* ln = new ListNode(
+      0, new ListNode(2, new ListNode(-5, new ListNode(5, new ListNode(10, new ListNode(-10))))));
+  Solution sol;
+  sol.sortLinkedList(ln);
+  free_list_node(ln);
+}
